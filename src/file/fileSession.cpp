ae1ebb05d395d76e3301c950b8f01d17626b5c61
Matt Richardson <matt.richardson@msrconsults.com>
2017-02-08 15:15:52 -0700

    Some necessary changes

--- src/file/fileSession.cpp
+++ src/file/fileSession.cpp
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   fileSession.cpp
  * Author: mrichardson
@@ -15,11 +9,11 @@
 namespace session {
 
     fileSession::fileSession(fileData fdata) : Session(fdata) {
-        fstream.open(fdata.main_path);
+        std::fstream.open(fdata.main_path);
     }
 
     fileSession::~fileSession() {
-        fstream.close();
+        std::fstream.close();
         
     }
 }
--- src/file/fileSession.h
+++ src/file/fileSession.h
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   fileSession.h
  * Author: mrichardson
@@ -15,17 +9,18 @@
 #define FILESESSION_H
 
 #include <fstream>
+#include <string>
 
 namespace session {
 
     struct fileData : sData{
-        string main_path = "";
+        std::string main_path = "";
         int max_size = 50;
     };
     class fileSession : public Session{
-        fstream fileRef;
+        std::fstream fileRef;
     public:
-        inline fstream get_ref(){return this->fileRef;};
+        inline std::fstream get_ref(){return this->fileRef;};
         fileSession(fileData fdata);
         virtual ~fileSession();
     private:
